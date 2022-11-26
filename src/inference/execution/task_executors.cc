73e275c63126ca6c1ac05911f1bd1834674b5b74
Geon-Woo Kim <gwkim@utexas.edu>
2022-11-26 05:36:59 +0000

    Parallelize input send/recv

--- src/inference/execution/task_executors.cc
+++ src/inference/execution/task_executors.cc
@@ -13,37 +13,36 @@ void input_bsend_fn(caf::blocking_actor* self,
                     const NDArray& dst_gnids,
                     const uint32_t tag) {
   TraceMe send(batch_id, "input_send");
-  auto fn = [&](const NDArray& arr) {
-    auto rh = self->request(mpi_actor, caf::infinite, caf::mpi_bsend_atom_v, arr, tag);
-    receive_result<bool>(rh);
-  };
 
-  fn(new_gnids);
-  fn(new_features);
-  fn(src_gnids);
-  fn(dst_gnids);
+  std::vector<caf::response_handle<caf::blocking_actor, caf::message, true>> rh_list;
+
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_bsend_atom_v, new_gnids, tag));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_bsend_atom_v, new_features, tag + 1));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_bsend_atom_v, src_gnids, tag + 2));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_bsend_atom_v, dst_gnids, tag + 3));
+  
+  for (int i = 0; i < rh_list.size(); i++) {
+    receive_result<bool>(rh_list[i]);
+  }
 
   self->receive([](caf::get_atom) { });
 }
 
 void input_brecv_fn(caf::blocking_actor* self, const caf::actor& mpi_actor, const uint32_t tag) {
+  std::vector<caf::response_handle<caf::blocking_actor, caf::message, true>> rh_list;
 
-  auto fn = [&]() {
-    auto rh = self->request(mpi_actor, caf::infinite, caf::mpi_brecv_atom_v, 0, tag);
-    return receive_result<NDArray>(rh);
-  };
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_brecv_atom_v, 0, tag));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_brecv_atom_v, 0, tag + 1));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_brecv_atom_v, 0, tag + 2));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_brecv_atom_v, 0, tag + 3));
 
-  NDArray new_gnids = fn();
-  NDArray new_features = fn();
-  NDArray src_gnids = fn();
-  NDArray dst_gnids = fn();
+
+  auto ret = std::vector<NDArray>();
+  for (int i = 0; i < rh_list.size(); i++) {
+    ret.push_back(receive_result<NDArray>(rh_list[i]));
+  }
 
   self->receive([=](caf::get_atom) {
-    auto ret = std::vector<NDArray>();
-    ret.push_back(std::move(new_gnids));
-    ret.push_back(std::move(new_features));
-    ret.push_back(std::move(src_gnids));
-    ret.push_back(std::move(dst_gnids));
     return ret;
   });
 }
@@ -58,36 +57,48 @@ void input_send_fn(caf::blocking_actor *self,
                    const NDArray& dst_gnids,
                    const uint32_t tag) {
   TraceMe send(batch_id, "input_send");
-  auto fn = [&](const NDArray& arr) {
-    auto rh = self->request(mpi_actor, caf::infinite, caf::mpi_send_atom_v, node_rank, arr, tag);
-    receive_result<bool>(rh);
-  };
 
-  fn(new_gnids);
-  fn(new_features);
-  fn(src_gnids);
-  fn(dst_gnids);
+  std::vector<caf::response_handle<caf::blocking_actor, caf::message, true>> rh_list;
+
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_send_atom_v, node_rank, new_gnids, tag));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_send_atom_v, node_rank, new_features, tag + 1));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_send_atom_v, node_rank, src_gnids, tag + 2));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_send_atom_v, node_rank, dst_gnids, tag + 3));
+  
+  for (int i = 0; i < rh_list.size(); i++) {
+    receive_result<bool>(rh_list[i]);
+  }
 }
 
 void input_recv_fn(caf::blocking_actor* self, const caf::actor& mpi_actor, const uint32_t tag) {
-  auto fn = [&]() {
-    auto rh = self->request(mpi_actor, caf::infinite, caf::mpi_recv_atom_v, 0, tag);
-    return receive_result<NDArray>(rh);
-  };
+  std::vector<caf::response_handle<caf::blocking_actor, caf::message, true>> rh_list;
 
-  NDArray new_gnids = fn();
-  NDArray new_features = fn();
-  NDArray src_gnids = fn();
-  NDArray dst_gnids = fn();
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_recv_atom_v, 0, tag));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_recv_atom_v, 0, tag + 1));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_recv_atom_v, 0, tag + 2));
+  rh_list.push_back(self->request(mpi_actor, caf::infinite, caf::mpi_recv_atom_v, 0, tag + 3));
+
+  auto ret = std::vector<NDArray>();
+  for (int i = 0; i < rh_list.size(); i++) {
+    ret.push_back(receive_result<NDArray>(rh_list[i]));
+  }
 
   self->receive([=](caf::get_atom) {
-    auto ret = std::vector<NDArray>();
-    ret.push_back(std::move(new_gnids));
-    ret.push_back(std::move(new_features));
-    ret.push_back(std::move(src_gnids));
-    ret.push_back(std::move(dst_gnids));
     return ret;
   });
+}
+
+void move_input_to_shared_mem_one_tensor(caf::blocking_actor *self,
+                                        const caf::actor& object_storage_actor,
+                                        int batch_id,
+                                        const std::string& name,
+                                        const NDArray& arr) {
+  auto rh = self->request(object_storage_actor, caf::infinite, caf::put_atom_v, name, arr);
+  receive_result<bool>(rh);
+  rh = self->request(object_storage_actor, caf::infinite, caf::move_to_shared_atom_v, name);
+  receive_result<bool>(rh);
+
+  self->receive([](caf::get_atom) {});
 }
 
 void move_input_to_shared_mem_fn(caf::blocking_actor *self,
@@ -98,17 +109,22 @@ void move_input_to_shared_mem_fn(caf::blocking_actor *self,
                                  const NDArray& src_gnids,
                                  const NDArray& dst_gnids) {
   TraceMe move_input(batch_id, "input_copy_to_shared_mem");
+  std::vector<caf::response_handle<caf::blocking_actor, caf::message, true>> rh_list;
+
   auto fn = [&](const std::string& name, const NDArray& arr) {
-    auto rh = self->request(object_storage_actor, caf::infinite, caf::put_atom_v, name, arr);
-    receive_result<bool>(rh);
-    rh = self->request(object_storage_actor, caf::infinite, caf::move_to_shared_atom_v, name);
-    receive_result<bool>(rh);
+    auto actor = self->spawn(move_input_to_shared_mem_one_tensor, object_storage_actor, batch_id, name, arr);
+    auto rh = self->request(actor, caf::infinite, caf::get_atom_v);
+    return rh;
   };
 
-  fn("new_gnids", new_gnids);
-  fn("new_features", new_features);
-  fn("src_gnids", src_gnids);
-  fn("dst_gnids", dst_gnids);
+  rh_list.push_back(fn("new_gnids", new_gnids));
+  rh_list.push_back(fn("new_features", new_features));
+  rh_list.push_back(fn("src_gnids", src_gnids));
+  rh_list.push_back(fn("dst_gnids", dst_gnids));
+
+  for (int i = 0; i < rh_list.size(); i++) {
+    receive_result<void>(rh_list[i]);
+  }
 
   self->receive([](caf::get_atom) { });
 }

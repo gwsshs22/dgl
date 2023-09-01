39c5c1830edf9abc9d865faf8b835bd2644f89f6
Geon-Woo Kim <gwkim@utexas.edu>
2023-09-01 01:21:30 +0000

    Change request_generator to use a separate thread for emuleating req arrivals

--- omega/serving/infer_requests.py
+++ omega/serving/infer_requests.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import Any
 from pathlib import Path
 import time
+import threading
+import queue
 
 import torch
 import numpy as np
@@ -17,12 +19,15 @@ class InferBatchRequest:
 
 class RequestGenerator:
 
-    def __init__(self, trace_dir, req_per_sec, arrival_type):
+    def __init__(self, trace_dir, req_per_sec, arrival_type, random_seed):
         self._load_traces(trace_dir)
         self._req_per_sec = req_per_sec
         self._arrival_type = arrival_type
-        self._idx = 0
-        self._last_time = None
+        self._random_seed = random_seed
+
+        self._num_reqs = None
+        self._req_gen_thread = None
+        self._queue = None
 
     def _load_traces(self, trace_dir):
         trace_dir = Path(trace_dir)
@@ -37,40 +42,55 @@ class RequestGenerator:
                 dst_gnids=tensors["dst_gnids"]
             ))
 
+
+    @staticmethod
+    def req_gen_thread_main(q, num_reqs, req_per_sec, arrival_type, random_seed):
+        np.random.seed(random_seed)
+        torch.manual_seed(random_seed)
+
+        for idx in range(num_reqs):
+            q.put(idx)
+            if req_per_sec <= 0.0:
+                continue
+            
+            if arrival_type == "poisson":
+                sleep_time = np.random.exponential(1 / req_per_sec)
+            else:
+                sleep_time = 1 / req_per_sec
+            time.sleep(sleep_time)
+
+        q.put(None)
+
+
+    def set_num_reqs(self, num_reqs):
+        self._num_reqs = num_reqs
+
     def __iter__(self):
-        self._idx = 0
-        self._last_time = None
+        self._queue = queue.SimpleQueue()
+        self._req_gen_thread = threading.Thread(
+            target=RequestGenerator.req_gen_thread_main,
+            args=(
+                self._queue,
+                self._num_reqs,
+                self._req_per_sec,
+                self._arrival_type,
+                self._random_seed
+            ),
+            daemon=True
+        )
+        self._req_gen_thread.start()
         return self
 
-    def _sleep(self):
-        if self._req_per_sec <= 0.0:
-            return
-        
-        if self._arrival_type == "poisson":
-            sleep_time = np.random.exponential(1 / self._req_per_sec)
-        else:
-            sleep_time = 1 / self._req_per_sec
-
-        if self._last_time is None:
-            time.sleep(sleep_time)
-            self._last_time = time.time()
-        else:
-            current_time = time.time()
-            elapsed = current_time - self._last_time
-            sleep_time -= elapsed
-            if sleep_time > 0:
-                time.sleep(sleep_time)
-                self._last_time = time.time()
-            else:
-                self._last_time = current_time
-            
-
     def __next__(self):
-        self._idx += 1
-        self._sleep()
+        idx = self._queue.get()
+        if idx is None:
+            self._queue = None
+            self._num_reqs = None
+            self._req_gen_thread = None
+            raise StopIteration
         
-        return self._batch_requests[self._idx % self._num_traces]
+        return self._batch_requests[idx % self._num_traces]
 
 
-def create_req_generator(trace_dir, req_per_sec, arrival_type):
-    return RequestGenerator(trace_dir, req_per_sec, arrival_type)
+def create_req_generator(trace_dir, req_per_sec, arrival_type, random_seed):
+    return RequestGenerator(trace_dir, req_per_sec, arrival_type, random_seed)

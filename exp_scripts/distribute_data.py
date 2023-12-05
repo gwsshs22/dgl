8581a9abd5ead27c4ea62322447cb716a1b268f8
gwkim <gwkim@utexas.edu>
2023-12-05 02:40:56 +0000

    Add more scripts for experiments

--- exp_scripts/distribute_data.py
+++ exp_scripts/distribute_data.py
@@ -4,20 +4,21 @@ import sys
 
 def main(args):
     num_parts = args.num_parts
+    graph_partitioning = args.graph_partitioning
     machine_ips = args.machine_ips.split(",")[:num_parts]
 
     for i, part_id in enumerate(range(num_parts)):
         if i == args.my_rank:
             continue
 
-        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-random-{num_parts}/{args.dataset}.json"
+        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-{graph_partitioning}-{num_parts}/{args.dataset}.json"
         os.system(f"rsync -avP --mkpath {target_path} {args.ssh_username}@{machine_ips[i]}:{target_path}")
-        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-random-{num_parts}/part{i}/"
+        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-{graph_partitioning}-{num_parts}/part{i}/"
         os.system(f"rsync -avP --mkpath {target_path} {args.ssh_username}@{machine_ips[i]}:{target_path}")
 
-        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-random-{num_parts}-outedges/{args.dataset}.json"
+        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-{graph_partitioning}-{num_parts}-outedges/{args.dataset}.json"
         os.system(f"rsync -avP --mkpath {target_path} {args.ssh_username}@{machine_ips[i]}:{target_path}")
-        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-random-{num_parts}-outedges/part{i}/"
+        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-{graph_partitioning}-{num_parts}-outedges/part{i}/"
         os.system(f"rsync -avP --mkpath {target_path} {args.ssh_username}@{machine_ips[i]}:{target_path}")
 
 if __name__ == "__main__":
@@ -26,6 +27,7 @@ if __name__ == "__main__":
     parser.add_argument('--num_parts', type=int)
     parser.add_argument('--machine_ips', type=str)
     parser.add_argument('--my_rank', type=int, default=-1)
+    parser.add_argument('--graph_partitioning', type=str, default='random')
     parser.add_argument('--ssh_username', type=str, default='gwkim')
 
     args = parser.parse_args()
--- exp_scripts/distribute_degrees.py
+++ exp_scripts/distribute_degrees.py
@@ -4,15 +4,16 @@ import sys
 
 def main(args):
     num_parts = args.num_parts
+    graph_partitioning = args.graph_partitioning
     machine_ips = args.machine_ips.split(",")[:num_parts]
 
     for i, part_id in enumerate(range(num_parts)):
         if i == args.my_rank:
             continue
 
-        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-random-{num_parts}/degrees.dgl"
+        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-{graph_partitioning}-{num_parts}/degrees.dgl"
         os.system(f"rsync -avP --mkpath {target_path} {args.ssh_username}@{machine_ips[i]}:{target_path}")
-        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-random-{num_parts}-outedges/degrees.dgl"
+        target_path = f"$DGL_DATA_HOME/omega_datasets-{num_parts}/{args.dataset}-{graph_partitioning}-{num_parts}-outedges/degrees.dgl"
         os.system(f"rsync -avP --mkpath {target_path} {args.ssh_username}@{machine_ips[i]}:{target_path}")
 
 if __name__ == "__main__":
@@ -21,6 +22,7 @@ if __name__ == "__main__":
     parser.add_argument('--num_parts', type=int)
     parser.add_argument('--machine_ips', type=str)
     parser.add_argument('--my_rank', type=int, default=-1)
+    parser.add_argument('--graph_partitioning', type=str, default='random')
     parser.add_argument('--ssh_username', type=str, default='gwkim')
 
     args = parser.parse_args()
